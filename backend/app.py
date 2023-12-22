56070c7db2b0c077ad10119a401680800c1c7822
Prahitha <prahitha.movva03@gmail.com>
2023-12-21 22:51:54 -0500

    get result from personalised prompt

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,90 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS, cross_origin
+from config import Config
+from openai import OpenAI
+import threading
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/hello', methods=['GET'])
+app.config.from_object(Config)
+client = OpenAI(api_key=app.config['OPENAI_API_KEY'])
+
+# Set the OpenAI API key from the configuration
+
+# Mutex for thread safety
+thread_lock = threading.Lock()
+
+@app.route('/', methods=['GET'])
 def hello():
     return jsonify(message="Hello, Flask!")
+
+
+@app.route('/home', methods=['POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/generate_text', methods=['POST'])
+@cross_origin(origins='*')
+def generate_text():
+    # Retrieve input data from the UI
+    try:
+        data = request.get_json()
+
+        pantry_items = data.get('pantry_items', [])
+        dietary_preferences = data.get('dietary_preferences', '')
+        allergies = data.get('allergies', [])
+        family_size = data.get('family_size', '')
+        cooking_time = data.get('cooking_time', '')
+        days_of_week = data.get('days_of_week', [])
+        meals_for_the_day = data.get('meals_for_the_day', '')
+
+        print("here")
+
+        generated_output = get_meal_suggestions_from_openai(pantry_items, dietary_preferences, allergies, family_size, cooking_time, days_of_week, meals_for_the_day)
+
+        return jsonify({'status': 'success', 'formatted_output': generated_output})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+def get_meal_suggestions_from_openai(pantry_items, dietary_preferences, allergies, family_size, cooking_time, days_of_week, meals_for_the_day):
+    # Construct a prompt for OpenAI GPT-3
+    prompt = f"Generate meal suggestions for {meals_for_the_day} with ingredients: {', '.join(pantry_items)}. Dietary preferences: {dietary_preferences}. Allergies: {', '.join(allergies)}. Family size: {family_size}. Cooking time: {cooking_time}. Days of Week: {', '.join(days_of_week)}."
+
+    prompt = f"""Generate meal suggestions for the following with nutritional information:
+                Pantry items: {', '.join(pantry_items)}
+                Dietary preferences: {dietary_preferences}
+                Allergies: {', '.join(allergies)}
+                Family size: {family_size}
+                Cooking time: {cooking_time}
+                Days of Week: {', '.join(days_of_week)}
+                Meals for the day: {meals_for_the_day}
+                Note: Do not shorten the response. I need the plan for all the days requested. Dietary preferences and allergies are to be strictly considered. 
+                I also want to only use the pantry items listed and no extra items unless they are common like salt, pepper, oil etc. 
+                For any extra ingredients that are absolutely necessary, mention them in bold
+                I want the output to be in the following structure:
+                Day
+                Meal Type
+                Name of the dish
+                Ingredients used
+                Recipe in steps with quantity required
+                Nutritional Information"""
+
+    # Make a request to OpenAI GPT-3
+    response = client.chat.completions.create(model="gpt-3.5-turbo", 
+                                              messages=[{"role": "user", "content": prompt}],
+                                              max_tokens=3000, 
+                                              temperature=0.8)
+
+    # Extract the generated text from the response
+    generated_plan = response.choices[0].message.content
+
+    # Return the generated text (need to process it further)
+    return generated_plan
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [pantryItems, setPantryItems] = useState('');
+  const [dietaryPreferences, setDietaryPreferences] = useState('');
+  const [allergies, setAllergies] = useState('');
+  const [familySize, setFamilySize] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+  const [daysOfWeek, setDaysOfWeek] = useState('');
+  const [mealsForTheDay, setMealsForTheDay] = useState('');
+  const [mealSuggestions, setMealSuggestions] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/hello')
-      .then(response => setMessage(response.data.message))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  const generateMealSuggestions = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/generate_text', {
+        pantry_items: pantryItems.split(',').map(item => item.trim()),
+        dietary_preferences: dietaryPreferences,
+        allergies: allergies.split(',').map(allergy => allergy.trim()),
+        family_size: familySize,
+        cooking_time: cookingTime,
+        days_of_week: daysOfWeek.split(',').map(day => day.trim()),
+        meals_for_the_day: mealsForTheDay,
+      });
+
+      setMealSuggestions(response.data);
+    } catch (error) {
+      console.error('Error generating meal suggestions:', error);
+    }
+  };
 
   return (
-    <div className="App">
-      <h1>{message}</h1>
+    <div>
+      <h1>Meal Planner</h1>
+      <label>
+        Pantry Items:
+        <input type="text" value={pantryItems} onChange={(e) => setPantryItems(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Dietary Preferences:
+        <input type="text" value={dietaryPreferences} onChange={(e) => setDietaryPreferences(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Allergies:
+        <input type="text" value={allergies} onChange={(e) => setAllergies(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Family Size:
+        <input type="text" value={familySize} onChange={(e) => setFamilySize(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Cooking Time:
+        <input type="text" value={cookingTime} onChange={(e) => setCookingTime(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Days of Week:
+        <input type="text" value={daysOfWeek} onChange={(e) => setDaysOfWeek(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Meals for the Day:
+        <input type="text" value={mealsForTheDay} onChange={(e) => setMealsForTheDay(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={generateMealSuggestions}>Generate Meal Suggestions</button>
+
+      <h2>Generated Meal Suggestions</h2>
+      <ul>
+        {mealSuggestions.map((suggestion, index) => (
+          <li key={index}>
+            <strong>{suggestion.day} - {suggestion.meal_type}</strong><br />
+            Dish: {suggestion.dish_name}<br />
+            Ingredients: {suggestion.ingredients.join(', ')}<br />
+            Recipe: {suggestion.recipe}<br />
+            Nutritional Information: Calories - {suggestion.nutritional_info.calories}, Protein - {suggestion.nutritional_info.protein}, Carbs - {suggestion.nutritional_info.carbs}, Fat - {suggestion.nutritional_info.fat}<br />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
